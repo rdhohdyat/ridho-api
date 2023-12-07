7b54ff20d9858627679fce86d2b05caf35027697
rdhohdyat <rdho.hdyat@gmail.com>
2023-12-07 09:26:59 +0700

    auth middleware

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
+Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class)->middleware('hak-akses');
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    Route::apiResource('/posts',
+        App\Http\Controllers\Api\PostController::class);
+});
+
+Route::post('login', [App\Http\Controllers\Api\AuthController::class, 'signin']);
+Route::post('register', [App\Http\Controllers\Api\AuthController::class, 'signup']);
+
+Route::get('/post', [App\Http\Controllers\Api\PostController::class, 'index'])->middleware(['auth:sanctum', 'hak-akses:admin']);
+Route::get('post/:id', [App\Http\Controllers\Api\PostController::class, 'show'])->middleware('auth:sanctum');
